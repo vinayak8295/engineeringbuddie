0e63dce2ebb9fb98a998dd3efcf1e9ca227ec423
AKSHAT564564 <57329437+AKSHAT564564@users.noreply.github.com>
2021-01-14 14:29:36 +0530

    Update firstpage.php

--- firstpage.php
+++ firstpage.php
@@ -46,6 +46,59 @@
 </div>
 </div>
 
+
+
+
+
+
+
+
+<section>
+  <div class="container">
+    <div class="row" style=" margin-left: -30%; padding-top: 30px;">
+    <!--   <div class="col-lg-12"> -->
+    <div class="card" style=" width:2000px; height:500px; border: 1px solid red;">
+    
+   <div class="jumbotron" style="background: url(pythonlogo.png) no-repeat; background-size: 100%;">
+    <div class="row">
+    <div class="col-sm-8">
+  <h1 class="display-4"><strong>COURSES</strong></h1>
+  <p class="lead"><strong>The most useful courses which helps you to grow more in your engineering career...</strong>
+</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+<div class="col-sm-4">
+<div class="card" style="width:300px; height:420px; background: url(FirstPage/taylor.png) no-repeat; ">
+  <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
+  <div class="card-body">
+    <h5 class="card-title display-4" style="text-align: center;"><strong>Softwares</strong></h5>
+   <div style="padding-top: 25px;">
+   <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong style="font-size:1.2em;">P</strong><strong>ython</strong> </button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Productivity</strong></button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>TextEditors</strong></button></div>
+    <div style="padding-top: 10px;">  
+     <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Browsers</strong></button></div>
+    <div style="padding-top: 10px;">   
+   
+     </div></div>
+</div>
+</div>
+</div>
+    </div>
+ 
+</div>
+</div>
+</div>
+</div>
+
+
+</section>
 <div class="container-fluid">
   <div class="row" style="padding-top: 20px;">
    
@@ -138,7 +191,14 @@
 </div>
 </div>
 </div>
-</div>
+ 
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+</p>
+
 
 
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
