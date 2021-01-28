47b93fa355c5a05c33c4f7cc9b71542584dd0758
AKSHAT564564 <57329437+AKSHAT564564@users.noreply.github.com>
2021-01-28 12:24:42 +0530

    abcd

---
+++ crouselcards.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="firstpage.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</head>
+<body>
+
+
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="margin-top:4.5%;">
+
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <section>
+  <div class="container">
+    <div class="row" style=" margin-left: -30%; padding-top: 30px;">
+    <!--   <div class="col-lg-12"> -->
+    <div class="card" style=" width:2000px; height:500px; border: 1px solid red;">
+    
+   <div class="jumbotron" style="background: url(pythonlogo.png) no-repeat; background-size: 100%;">
+    <div class="row">
+    <div class="col-sm-8">
+  <h1 class="display-4"><strong>COURSES</strong></h1>
+  <p class="lead"><strong>The most useful courses which helps you to grow more in your engineering career...</strong>
+</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+<div class="col-sm-4 ">
+<div class="card" style="width:300px; height:420px; background: url(FirstPage/taylor.png) no-repeat; ">
+  <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
+  <div class="card-body">
+    <h5 class="card-title display-4" style="text-align: center;"><strong>Softwares</strong></h5>
+   <div style="padding-top: 25px;">
+   <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong style="font-size:1.2em;">P</strong><strong>ython</strong> </button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Productivity</strong></button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>TextEditors</strong></button></div>
+    <div style="padding-top: 10px;">  
+     <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Browsers</strong></button></div>
+    <div style="padding-top: 10px;">   
+   
+     </div></div>
+</div>
+</div>
+</div>
+    </div>
+ 
+</div>
+</div>
+</div>
+</div>
+
+
+</section>
+    </div>
+    <div class="carousel-item">
+     <section>
+  <div class="container">
+    <div class="row" style=" margin-left: -30%; padding-top: 30px;">
+    <!--   <div class="col-lg-12"> -->
+    <div class="card" style=" width:2000px; height:500px; border: 1px solid red;">
+    
+   <div class="jumbotron" style="background: url(pythonlogo.png) no-repeat; background-size: 100%;">
+    <div class="row">
+    <div class="col-sm-8">
+  <h1 class="display-4"><strong>COURSES</strong></h1>
+  <p class="lead"><strong>The most useful courses which helps you to grow more in your engineering career...</strong>
+</p>
+  <hr class="my-4">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+<div class="col-sm-4 ">
+<div class="card" style="width:300px; height:420px; background: url(FirstPage/taylor.png) no-repeat; ">
+  <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
+  <div class="card-body">
+    <h5 class="card-title display-4" style="text-align: center;"><strong>Softwares</strong></h5>
+   <div style="padding-top: 25px;">
+   <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong style="font-size:1.2em;">P</strong><strong>ython</strong> </button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Productivity</strong></button></div>
+    <div style="padding-top: 10px;">    
+    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>TextEditors</strong></button></div>
+    <div style="padding-top: 10px;">  
+     <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Browsers</strong></button></div>
+    <div style="padding-top: 10px;">   
+   
+     </div></div>
+</div>
+</div>
+</div>
+    </div>
+ 
+</div>
+</div>
+</div>
+</div>
+
+
+</section>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="FirstPage/visual.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+
+
+
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="NavBar/firstpage.js"></script>
+</body>
+</html>
--- firstpage.php
+++ firstpage.php
@@ -10,6 +10,14 @@
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<style type="text/css">
+  .col-md-6{
+    padding-top: 20px;
+  }
+  .doublecards{
+    padding-left: 50px;
+  }
+</style>
 </head>
 <body >
 <?php include 'NavBar/header_firstpage.php' ?>
@@ -53,56 +61,14 @@
 
 
 
-<section>
-  <div class="container">
-    <div class="row" style=" margin-left: -30%; padding-top: 30px;">
-    <!--   <div class="col-lg-12"> -->
-    <div class="card" style=" width:2000px; height:500px; border: 1px solid red;">
-    
-   <div class="jumbotron" style="background: url(pythonlogo.png) no-repeat; background-size: 100%;">
-    <div class="row">
-    <div class="col-sm-8">
-  <h1 class="display-4"><strong>COURSES</strong></h1>
-  <p class="lead"><strong>The most useful courses which helps you to grow more in your engineering career...</strong>
-</p>
-  <hr class="my-4">
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
-<div class="col-sm-4">
-<div class="card" style="width:300px; height:420px; background: url(FirstPage/taylor.png) no-repeat; ">
-  <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
-  <div class="card-body">
-    <h5 class="card-title display-4" style="text-align: center;"><strong>Softwares</strong></h5>
-   <div style="padding-top: 25px;">
-   <div style="padding-top: 10px;">    
-    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong style="font-size:1.2em;">P</strong><strong>ython</strong> </button></div>
-    <div style="padding-top: 10px;">    
-    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Productivity</strong></button></div>
-    <div style="padding-top: 10px;">    
-    <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>TextEditors</strong></button></div>
-    <div style="padding-top: 10px;">  
-     <button type="button" class="btn btn-light btn-block" style="padding: 10px;"><strong>Browsers</strong></button></div>
-    <div style="padding-top: 10px;">   
-   
-     </div></div>
-</div>
-</div>
-</div>
-    </div>
- 
-</div>
-</div>
-</div>
-</div>
+<?php include 'crouselcards.php' ?>
 
 
-</section>
+
 <div class="container-fluid">
   <div class="row" style="padding-top: 20px;">
    
-    <div class="col-lg-3" >
+    <div class="col-lg-3 col-md-6" >
 <div class="card" style="width:300px; height:420px; background: url(VPN.jpg) no-repeat;">
   <div class="card-body">
     <h5 class="card-title display-4" style="text-align: center;"><strong>Courses</strong></h5>
@@ -122,7 +88,7 @@
 </div>
 </div>
 
-    <div class="col-lg-3" style="margin-left: -30px;">
+    <div class="col-lg-3 col-md-6" style="">
 <div class="card" style="width:300px; height:420px; background: url(FirstPage/taylor.png) no-repeat; ">
   <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
   <div class="card-body">
@@ -142,7 +108,7 @@
 </div>
 </div>
 </div>
-   <div class="col-lg-3" style="margin-left: -30px;" >
+   <div class="col-lg-3 col-md-6" style="" >
 <div class="card" style="width:300px; height:420px; background: url(FirstPage/VPN.jpg) no-repeat; background-size: ">
   <!-- <img class="card-img-top" src="antivirus.jpg" alt="Card image cap"> -->
   <div class="card-body">
@@ -163,7 +129,7 @@
 </div>
 </div>
 
-<div style="padding-left:20px;">
+<div style="padding-top: 20px;">
    <div class="col-lg-3">
 <div class="card" style="width:300px; height:200px;">
   <img class="card-img-top" src="Adobe.png" style="height: 30%;" alt="Card image cap">
@@ -192,14 +158,11 @@
 </div>
 </div>
  
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-Why do we use it?
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-</p>
-
-
+<footer style="text-align: right; height: 50px; padding: 3px; background-color: DarkSalmon; color: white;">
+ <li>ABCd</li>
+ <li>aBCD</li>
+</footer>
 
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="NavBar/firstpage.js"></script>
